0b44c9e036cbdab1de3184c2397945521068cf46
Matt Kingshott <51963402+mattkingshott@users.noreply.github.com>
2022-07-23 10:15:44 +0100

    upgraded to vite 3

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue/dist/index.js';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({ plugins : [vue()] });
