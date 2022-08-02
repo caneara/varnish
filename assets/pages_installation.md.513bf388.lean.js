6d548c25099b953550bc8605f8aea4531df4e086
mattkingshott <mattkingshott@users.noreply.github.com>
2022-08-02 16:47:46 +0000

    deploy: 67c6ff96edf0d3fe746bbb9bcfe19d5a26f706c3

---
+++ assets/pages_installation.md.513bf388.lean.js
@@ -0,0 +1,1 @@
+import{_ as a,o as e,c as n,d as s}from"./app.756055f6.js";const f=JSON.parse('{"title":"Installation","description":"","frontmatter":{},"headers":[],"relativePath":"pages/installation.md"}'),t={name:"pages/installation.md"},o=s("",6),i=[o];function l(r,c,p,d,_,h){return e(),n("div",null,i)}var g=a(t,[["render",l]]);export{f as __pageData,g as default};
